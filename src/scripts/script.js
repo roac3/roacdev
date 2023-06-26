c03e8d8a16503420b436c486103448ebb915dd51
roac3 <carlosale0312@gmail.com>
2023-06-25 22:51:11 -0300

    type animation

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,18 +1,24 @@
-addEventListener('load',()=>{
-    const $card = document.querySelector('.cajita')
-
-    function animationUp(){
-        var windowheight = window.innerHeight;
-        var pPoint = $card.getBoundingClientRect().top;
-        var animationpoint = 250;
-
-        if (pPoint < windowheight - animationpoint){
-            $card.classList.add('animate-fade-up')
-        }else{
-            $card.classList.remove('animate-fade-up')
-        }
+const $card = document.querySelector('.cajita')
+const $text_code = document.querySelector('pre');
     
+
+function animationUp(){
+    var windowheight = window.innerHeight;
+    var pPoint = $card.getBoundingClientRect().top;
+    var animationpoint = 250;
+
+    if (pPoint < windowheight - animationpoint){
+        $card.classList.add('animate-fade-up')
+    }else{
+        $card.classList.remove('animate-fade-up')
     }
     
-    addEventListener('scroll',animationUp)
-});
+}
+
+let text = ($text_code.innerText === undefined) ? $text_code.innerText : $text_code.textContent;
+let cutText = text.split(/\r?\n/);
+
+console.log(cutText);
+    
+addEventListener('scroll',animationUp)
+
